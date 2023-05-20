bc4d1b95e0d0cb4f5fec755ed8948839856bfae4
Gibran <cambrongibran@gmail.com>
2023-05-20 00:07:21 -0600

    sixth commit

--- src/components/data/dummydata.js
+++ src/components/data/dummydata.js
@@ -125,7 +125,7 @@ export const portfolio = [
     category: "development",
     title: "Brex Logo",
     githubLink: "https://github.com/Favorableasp599/Proyecto-6-Portafolio",
-    link: "https://dulcet-cocada-868247.netlify.app/",
+    link: "https://aquamarine-hamster-cd0c78.netlify.app",
   },
 ];
 export const contact = [
